e706538c30d084c5ee2e8b8771131efb39db75c2
Mateja Lasan <m.vuk.lasan@icloud.com>
2021-03-06 22:44:01 -0500

    Allow OPTIONS in all routes

--- app/routes.py
+++ app/routes.py
@@ -8,9 +8,29 @@ from app import endpoints
 
 # Routes for the application.
 ROUTES = [
-  Route("/ping", endpoint=endpoints.ping, methods=["GET"], name="ping"),
-  Route("/qod", endpoint=endpoints.qod, methods=["GET"], name="qod"),
-  Route("/quotes", endpoint=endpoints.quotes, methods=["GET"], name="quotes"),
-  Route("/vote", endpoint=endpoints.vote, methods=["POST"], name="vote"),
-  Route("/submit", endpoint=endpoints.submit, methods=["POST"], name="submit"),
+  Route(
+    "/ping",
+    endpoint=endpoints.ping,
+    methods=["GET", "OPTIONS"],
+    name="ping"),
+  Route(
+    "/qod",
+    endpoint=endpoints.qod,
+    methods=["GET", "OPTIONS"],
+    name="qod"),
+  Route(
+    "/quotes",
+    endpoint=endpoints.quotes,
+    methods=["GET", "OPTIONS"],
+    name="quotes"),
+  Route(
+    "/vote",
+    endpoint=endpoints.vote,
+    methods=["POST", "OPTIONS"],
+    name="vote"),
+  Route(
+    "/submit",
+    endpoint=endpoints.submit,
+    methods=["POST", "OPTIONS"],
+    name="submit"),
 ]
