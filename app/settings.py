ea91644f7389cfc60e6e145d31a35f66022f974e
Mateja Lasan <m.vuk.lasan@icloud.com>
2021-03-19 15:16:33 -0400

    Add Sentry error tracking

--- app/settings.py
+++ app/settings.py
@@ -4,10 +4,16 @@ One unified place to hold configurations for the application.
 """
 
 import typing
+import sentry_sdk 
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 from app import utils
+
+
+# Initialize the Sentry SDK.
+sentry_sdk.init(dsn=utils.config("SENTRY_DSN", default=None))
 
 
 # Load whether or not to run the application in debug mode from config.
@@ -16,6 +22,8 @@ DEBUG = utils.config("DEBUG", cast=bool, default=False)
 
 # Load any middleware to run with the application.
 MIDDLEWARE: typing.Sequence[Middleware] = [
+  Middleware(
+    SentryAsgiMiddleware),
   Middleware(
     CORSMiddleware,
     allow_origins=["*"],
