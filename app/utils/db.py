9de01b84dfdeac4142f0796af27ff2f0b1615934
Mateja <matootie@icloud.com>
2021-05-10 14:38:45 -0400

    Simplify deployment process
    Include packages that the server depends on in base packages, even if
    they aren't used in production. Packages like requests are used for
    test client but need to be included in base dependencies to resolve
    version conflicts. Separating aerich at that point just creates more
    hassle than it solves because the workflow will have to build two
    images, one with aerich to use in jobs and one without to use in
    production.

--- app/utils/db.py
+++ app/utils/db.py
@@ -1,24 +1,24 @@
 from app.utils import config
 
 TORTOISE_ORM = {
-  'connections': {
-    'default': {
-      'engine': 'tortoise.backends.asyncpg',
-      'credentials': {
-        'host': config("DB_HOST"),
-        'port': config("DB_PORT"),
-        'user': config("DB_USER"),
-        'password': config("DB_PASS"),
-        'database': config("DB_NAME"),
+  "connections": {
+    "default": {
+      "engine": "tortoise.backends.asyncpg",
+      "credentials": {
+        "host": config("DB_HOST"),
+        "port": config("DB_PORT"),
+        "user": config("DB_USER"),
+        "password": config("DB_PASS"),
+        "database": config("DB_NAME"),
       }
     },
   },
-  'apps': {
-    'models': {
-      'models': ['app.services.quotes.models'],
-      'default_connection': 'default',
+  "apps": {
+    "models": {
+      "models": ["app.services.quotes.models", "aerich.models"],
+      "default_connection": "default",
     }
   },
-  'use_tz': False,
-  'timezone': 'UTC'
+  "use_tz": False,
+  "timezone": "UTC"
 }
