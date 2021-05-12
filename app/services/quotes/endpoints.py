6cdd7c9bce304bb76657ec80e1c77b66e7c3c489
Mateja Lasan <m.vuk.lasan@icloud.com>
2021-05-12 12:55:59 -0400

    Protect create Meaning endpoint
    You can only create a Meaning for a Quote if you are friends with the
    author or if you currently have the Quote on your voting ballot.

--- app/services/quotes/endpoints.py
+++ app/services/quotes/endpoints.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from aioredis import Redis
 from starlette.requests import Request
 from starlette.responses import JSONResponse
@@ -145,11 +146,13 @@ async def get_all_meanings(request: Request, user: User):
 
 @requires_auth
 @uses_user
-async def create_meaning(request: Request, user: User):
+@uses_redis
+async def create_meaning(request: Request, redis: Redis, user: User):
   """Create a new Meaning for a Quote.
   """
 
   quote_id = request.path_params["quote_id"]
+  day = datetime.utcnow().date().isoformat()
 
   quote: Quote = await Quote.filter(id=quote_id).first()
 
@@ -164,6 +167,15 @@ async def create_meaning(request: Request, user: User):
       {"message": "You cannot write Meanings for your own Quote."},
       status_code=403,
     )
+  
+  friends = await redis.sismember(f"friends:{user.user_id}", quote.author_id)
+  ballot = await redis.sscan(f"today:{day}:request:{user.user_id}")
+  on_ballot = quote_id in ballot
+
+  if not friends or not on_ballot:
+    return JSONResponse({
+      "message": "You are not permitted to create a Meaning for this quote."
+    }, status_code=403)
 
   existing: Meaning = await Meaning.filter(
     author_id=user.user_id, quote=quote).first()
